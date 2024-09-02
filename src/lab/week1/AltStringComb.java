c7c731b941fe5fabdb7d4cdd05b21be7df880394
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2024-09-02 23:34:48 +0530

    02-09-24-1

--- src/lab/week1/AltStringComb.java
+++ src/lab/week1/AltStringComb.java
@@ -1,28 +1,66 @@
+//package lab.week1;
+//import java.util.Scanner;
+//public class AltStringComb
+//{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter 1 string");
+//        String s1 = sc.nextLine();
+//        System.out.println("enter 2 string");
+//        String s2 = sc.nextLine();
+//        StringBuilder comb = new StringBuilder();
+//        int l1=s1.length();
+//        int l2=s2.length();
+//        int maxl=Math.max(l1,l2);
+//        for (int i=0;i<maxl;i++)
+//        {
+//            if(i<l1)
+//            {
+//             comb.append(s1.charAt(i));
+//            }
+//            if(i<l2)
+//            {
+//                comb.append(s2.charAt(i));
+//            }
+//        }
+//        System.out.println(comb.toString());
+//    }
+//}
+//
+//
+//
+//------------------------------
 package lab.week1;
 import java.util.Scanner;
-public class AltStringComb
-{
+
+public class AltStringComb {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter 1 string");
-        String s1 = sc.nextLine();
-        System.out.println("enter 2 string");
-        String s2 = sc.nextLine();
+        System.out.println("Enter strings separated by commas:");
+        String input = sc.nextLine();
+        String[] strings = input.split(",");
+
+        if (strings.length < 2) {
+            System.out.println("Please enter at least two strings separated by commas.");
+            return;
+        }
+
+        String s1 = strings[0].trim();
+        String s2 = strings[1].trim();
         StringBuilder comb = new StringBuilder();
-        int l1=s1.length();
-        int l2=s2.length();
-        int maxl=Math.max(l1,l2);
-        for (int i=0;i<maxl;i++)
-        {
-            if(i<l1)
-            {
-             comb.append(s1.charAt(i));
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int maxl = Math.max(l1, l2);
+
+        for (int i = 0; i < maxl; i++) {
+            if (i < l1) {
+                comb.append(s1.charAt(i));
             }
-            if(i<l2)
-            {
+            if (i < l2) {
                 comb.append(s2.charAt(i));
             }
         }
+
         System.out.println(comb.toString());
     }
 }
--- src/lab/week2/UniformBinarySearch.java
+++ src/lab/week2/UniformBinarySearch.java
@@ -1,7 +1,68 @@
 package lab.week2;
+import java.util.Scanner;
 
-public class UniformBinarySearch {
+public class UniformBinarySearch
+{
+    static int MAX_SIZE = 1000;
+    static int lookup_table[] = new int[MAX_SIZE];
+
+    static void create_table(int n)
+    {
+        int pow = 1;
+        int co = 0;
+        do
+        {
+            pow <<= 1;
+            lookup_table[co] = (n + (pow >> 1)) / pow;
+        }
+        while (lookup_table[co++] != 0);
+
+        // Print the lookup table
+        System.out.println("Lookup Table:");
+        for (int i = 0; i < co; i++) {
+            System.out.print(lookup_table[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static int binary(int arr[], int v)
+    {
+        int index = lookup_table[0] - 1;
+        int co = 0;
+        while (lookup_table[co] != 0)
+        {
+            if (v == arr[index])
+                return index;
+            else if (v < arr[index])
+            {
+                index -= lookup_table[++co];
+            }
+            else
+            {
+                index += lookup_table[++co];
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array elements separated by commas:");
+        String input = sc.nextLine();
+
+        // Split the input string into an array of strings
+        String[] elements = input.split(",");
+
+        // Convert the strings to integers
+        int[] arr = new int[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            arr[i] = Integer.parseInt(elements[i].trim());
+        }
+        int n = arr.length;
+        System.out.println("Enter the key: ");
+        int key = sc.nextInt();
+        create_table(n);
+        System.out.println("Position of " + key + " in array = " + binary(arr, key));
+    }
 }
-
-
-
