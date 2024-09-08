52c1982f8373b2eee0a9d57c0ed3aa3d60117d7a
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2024-09-08 19:32:58 +0530

    08-09-24-1

--- src/lab/week2/RecursiveLinearSearch.java
+++ src/lab/week2/RecursiveLinearSearch.java
@@ -1,8 +1,57 @@
+//package lab.week2;
+//import java.util.Arrays;
+//import java.util.Scanner;
+//public class RecursiveLinearSearch
+//{
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the array elements separated by commas:");
+//        String input = scanner.nextLine();
+//
+//        // Split the input string into an array of strings
+//        String[] elements = input.split(",");
+//
+//        // Convert the strings to integers
+//        int[] array = new int[elements.length];
+//        for (int i = 0; i < elements.length; i++) {
+//            array[i] = Integer.parseInt(elements[i].trim());
+//        }
+//
+//        System.out.println("The array is: " + Arrays.toString(array));
+//        System.out.println("enter key:");
+//        int key= scanner.nextInt();
+//        int len= input.length();
+//        int result= recursiveLinearSearch(array, key, len);
+//        if(result == -1)
+//        {
+//            System.out.println("Element not found");
+//        }
+//        else
+//        {
+//            System.out.println("Element found at index: " + result);
+//        }
+//    }
+//    public static int recursiveLinearSearch(int[] arr, int key, int len)
+//    {
+//        for (int i = 0; i < len; i++)
+//        {
+//            if(arr[i] == key)
+//                return i;
+//        }
+//        return -1;
+//    }
+//}
+//
+//
+//
+//
+//
 package lab.week2;
 import java.util.Arrays;
 import java.util.Scanner;
-public class RecursiveLinearSearch
-{
+
+public class RecursiveLinearSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,31 +68,24 @@ public class RecursiveLinearSearch
         }
 
         System.out.println("The array is: " + Arrays.toString(array));
-        System.out.println("enter key:");
-        int key= scanner.nextInt();
-        int len= input.length();
-        int result= recursiveLinearSearch(array, key, len);
-        if(result == -1)
-        {
+        System.out.println("Enter key:");
+        int key = scanner.nextInt();
+
+        int result = recursiveLinearSearch(array, key, 0);
+        if (result == -1) {
             System.out.println("Element not found");
-        }
-        else
-        {
+        } else {
             System.out.println("Element found at index: " + result);
         }
     }
-    public static int recursiveLinearSearch(int[] arr, int key, int len)
-    {
-        for (int i = 0; i < len; i++)
-        {
-            if(arr[i] == key)
-                return i;
+
+    public static int recursiveLinearSearch(int[] arr, int key, int index) {
+        if (index >= arr.length) {
+            return -1; // Base case: key not found
         }
-        return -1;
+        if (arr[index] == key) {
+            return index; // Base case: key found
+        }
+        return recursiveLinearSearch(arr, key, index + 1); // Recursive case
     }
 }
-
-
-
-
-
